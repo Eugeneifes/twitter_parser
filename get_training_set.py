0c9f2eab8d128ad82c97dfb82ef4bda3a33e5b6c
Eugeneifes <Eugeneifes1812@gmail.com>
2016-04-19 17:53:34 +0300

    Update get_training_set.py

--- get_training_set.py
+++ get_training_set.py
@@ -1,19 +1,11 @@
 __author__ = 'Eugene'
 # -*- coding: utf-8 -*-
 
-
-
 import tweepy
 import csv
 import hashlib
 import os.path
 from datetime import datetime
-from dateutil import parser
-import email.utils
-
-
-
-
 
 
 consumer_key = ""
@@ -30,81 +22,71 @@ happy = [":)", ":D", ":-)"]
 sad = [":(", ":-("]
 
 
-def load_hashes():
+#Загрузка последнего состояния из БД
+def get_last_state():
     hash_set = []
+    id_set = []
+    time_set = []
+
     with open("training_set.csv", "r") as csvfile:
         reader = csv.DictReader(csvfile)
         for line in reader:
             hash_set.append(line["hash"])
-    print("%s hashes loaded" % len(hash_set))
-    return hash_set
+            id_set.append(line["id"])
+            time_set.append(line["date"])
+
+    print("%s tweets loaded" % len(hash_set))
+    minim = min(id_set)
+    maxim = max(id_set)
+    mintime = time_set[id_set.index(minim)]
+    maxtime = time_set[id_set.index(maxim)]
+
+    return hash_set, minim, maxim, mintime, maxtime
 
 
-def get_last_state():
-    with open("training_set.csv", "r") as csvfile:
-        last = csvfile.readlines()[1]
-        last = last.split(",")
-        min = int(last[0])
-        date = last[1]
-        return (min, date)
-
+#конвертируем дату в удобочитаемый вид
 def date_converter(date):
 
-    date_tz = email.utils.parsedate_tz(date)
-    mydate=datetime(*date_tz[:6])
-    return mydate
+    d = datetime.strptime(date, '%a %b %d %H:%M:%S %z %Y')
+    d = d.strftime('%Y-%m-%d %H:%M')
+    return d
 
 
+#подготовка начального состояния для загрузки
 def presetting():
     #Check database existance
     if os.path.isfile("training_set.csv"):
-        hash_set = load_hashes()
+
+        hash_set, minim, maxim, mintime, maxtime = get_last_state()
         tweetCount = len(hash_set)
-        last_id, last_date = get_last_state()
+
 
         print("\n")
         print("Presetting:")
         print("Tweets in database: %d" % tweetCount)
-        print("Last id: %s" % last_id)
-        print("Last date: %s" % last_date)
+        print("max id: %s" % maxim)
+        print("max date: %s" % maxtime)
+        print("min id: %s" % minim)
+        print("min date: %s" % mintime)
+        print("Distance between max and min ID: %d" % (int(maxim) - int(minim)))
         print("\n")
 
     else:
         print("no database")
+        print("creating database")
         hash_set = []
         with open("training_set.csv", "w") as csvfile:
             fieldnames = ["id", "date", "text", "hash", "polarity"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writeheader()
             tweetCount = 0
-            last_id = None
+            minim = None
 
-    return (tweetCount, last_id, hash_set)
-
-
-
-def get_tweets(query):
-
-    tweetCount, max_id, hash_set = presetting()
-
-    #While not enough tweets in database
-    while tweetCount < maxTweets:
-
-        duplicates = 0
-        undecodable = 0
-
-        if max_id is None:
-            tweets = api.search(q=query, count=100, lang="en")
-        else:
-            tweets = api.search(q=query, count=100, lang="en", max_id=str(int(max_id)-1))
-        print("%s tweets in queue" % len(tweets))
-
-        if len(tweets) == 0:
-            print("No tweets for such query")
-            break
+    return (tweetCount, minim, hash_set)
 
 
-        with open("training_set.csv", "a") as csvfile:
+def put_to_db(tweets, hash_set, duplicates, undecodable, tweetCount):
+    with open("training_set.csv", "a") as csvfile:
             fieldnames = ["id", "date", "text", "hash", "polarity"]
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             for tweet in tweets:
@@ -133,18 +115,45 @@ def get_tweets(query):
                         tweetCount += 1
                     except:
                         undecodable += 1
+    return (duplicates, undecodable, tweetCount)
 
-            print("\n")
-            print("Statistics per step:")
-            print("%d duplicates" % duplicates)
-            print("%d undecodable" % undecodable)
-            print("%d new tweets" % (len(tweets)-duplicates-undecodable))
-            print("ongoing tweet count: %d" % tweetCount)
-            print("\n")
 
-            if (len(tweets)-duplicates-undecodable) == 0:
-                print("Can't get any new tweets")
-                break
+def get_tweets(query):
+
+    tweetCount, minim, hash_set = presetting()
+
+    #While not enough tweets in database
+    while tweetCount < maxTweets:
+
+        duplicates = 0
+        undecodable = 0
+
+        if minim is None:
+            tweets = api.search(q=query, count=100, lang="en")
+        else:
+            tweets = api.search(q=query, count=100, lang="en", max_id=str(int(minim)-1))
+        print("%s tweets in queue" % len(tweets))
+
+        if len(tweets) == 0:
+            print("No tweets for such query")
+            break
+
+        duplicates, undecodable, tweetCount = put_to_db(tweets, hash_set, duplicates, undecodable, tweetCount)
+
+
+        print("\n")
+        print("Statistics per step:")
+        print("%d duplicates" % duplicates)
+        print("%d undecodable" % undecodable)
+        print("%d new tweets" % (len(tweets)-duplicates-undecodable))
+        print("ongoing tweet count: %d" % tweetCount)
+        print("\n")
+
+
+        if (len(tweets)-duplicates-undecodable) == 0:
+            print("Can't get any new tweets")
+            
+        minim = tweets[len(tweets)-1]._json["id"]
 
 
 get_tweets(":) OR :(")
